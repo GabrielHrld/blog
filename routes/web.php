f0bf3e84629cfd020083e3610f901cfa77a9b29b
Gabi <g.federico.rodriguez@gmail.com>
2021-01-27 17:19:32 -0300

    index view and actions buttons

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('posts', 'Backend\PostController')
+->middleware('auth')
+->except('show');
